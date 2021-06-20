4f09a4e3a56a6a994fb13408b9d074a0a65e2b65
PavloSkrebets <pavelskrebec@gmail.comgit config --global user.email pavelskrebec@gmail.comexitqq!quirgit config --global user.email pavelskrebec@gmail.com>
2021-06-20 16:59:57 +0300

    test

---
+++ script.js
@@ -0,0 +1,23 @@
+<script>
+            StackExchange.ready(function () {
+
+                    StackExchange.using("snippets", function () {
+                        StackExchange.snippets.initSnippetRenderer();
+                    });
+                    
+                StackExchange.using("postValidation", function () {
+                    StackExchange.postValidation.initOnBlurAndSubmit($('#post-form'), 2, 'answer');
+                });
+
+
+                StackExchange.question.init({showAnswerHelp:true,totalCommentCount:7,shownCommentCount:5,enableTables:true,questionId:9362692});
+
+                styleCode();
+
+                    StackExchange.realtime.subscribeToQuestion('1', '9362692');
+                    StackExchange.using("gps", function () { StackExchange.gps.trackOutboundClicks('#content', '.js-post-body'); });
+
+
+
+            });
+        </script>
